c693b875ebf4e1a5548eb3ea16296455d9214c73
MariaVictoria00 <mariavictoriapaybe@gmail.com>
2021-04-29 01:32:13 -0400

    acessa e exibi dados do bd

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+
+//conetca com seu banco de dados local
+$link = mysqli_connect("localhost", "root", "liam02", "Noticias");
+
+// verifica se fez a conexão com o bd
+if (mysqli_connect_errno()) {
+    printf("Erro ao conectar no banco de dado: %s\n", mysqli_connect_error());
+    exit();
+}
+
+// seleciona a base de dados em que vamos trabalhar
+$query = "SELECT * FROM noticias";
+
+if ($result = mysqli_query($link, $query)) {
+
+// busca matriz associativa 
+    while ($row = mysqli_fetch_assoc($result)) {   
+       printf ("%s (%s) %s %s\n",$oJson->id = $row["id"],$row["titulo"], $row["data"], $row["texto"]);
+    }
+
+    //mostrar resultados
+    mysqli_free_result($result);
+}
+?>
---
+++ orientadoObjeto.php
@@ -0,0 +1,33 @@
+<?php
+
+//conetca com seu banco de dados local
+$mysqli = new mysqli("localhost", "root", "liam02", "Noticias");
+
+//cria um array
+$Array = array();
+
+// verifica se fez a conexão com o bd
+if ($mysqli->connect_errno) {
+    printf("Erro ao conectar no banco de dado: %s\n", $mysqli->connect_error);
+    exit();
+}
+
+// seleciona a base de dados em que vamos trabalhar
+$query = "SELECT * FROM noticias";
+
+
+if ($result = $mysqli->query($query)) {
+  
+// busca matriz associativa 
+    while ($row = $result->fetch_array(MYSQLI_ASSOC)) {
+        $Array[] = $row;
+    }
+
+    //retorna em json
+    echo  json_encode($Array);
+    
+
+     //mostrar resultados
+    $result->free();
+    
+}
